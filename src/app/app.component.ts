76b9d787446752256d1705ed75611eb017e4ddf8
Robert Hubley <rhubley@systemsbiology.org>
2019-02-25 13:57:20 -0800

    - Added Google Analytics to site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'dfam-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dfam';
+
+  constructor(private router: Router) {
+     this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (<any>window).gtag('config', 'UA-135151408-1', {'page_path': event.urlAfterRedirects });
+      }
+    });
+  }
+
 }
