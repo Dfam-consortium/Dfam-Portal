a50632cf5a5e88262ec4ea5f92b51043a6fcdd63
Jeb Rosen <jeb.rosen@systemsbiology.org>
2020-01-13 09:46:23 -0800

    Remove the (outdated) outage warning.
    This reverts commit cb85671d6afeecaa8d4a0e916c87d3e975c8382f.

--- src/app/shared/layout/public-footer.component.ts
+++ src/app/shared/layout/public-footer.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'dfam-public-layout-footer',
-  styleUrls: ['./public-footer.component.scss'],
   templateUrl: './public-footer.component.html'
 })
 export class PublicFooterComponent {
