bfeb30fc35ab7d59c7bb44c51c1f292b7550c5e3
Jeb Rosen <jeb.rosen@isbscience.org>
2020-06-22 12:49:27 -0700

    Add a notice for upcoming network maintenance.

--- src/app/shared/layout/public-footer.component.ts
+++ src/app/shared/layout/public-footer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'dfam-public-layout-footer',
+  styleUrls: ['./public-footer.component.scss'],
   templateUrl: './public-footer.component.html'
 })
 export class PublicFooterComponent {
