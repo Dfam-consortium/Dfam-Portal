Dfam-consortium/Dfam-Portal

commit df235fec06ca8f0570b7134530623b0704e6888a

change(browse): check Descendants option by default
This is a more sensible default for most searches with the current dataset.

src/app/shared/browse-panel/browse-panel.component.ts
@@ -203,7 +203,7 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
       this.search.clade_ancestors = initialCladeA;
       this.searchApiOptions.clade_ancestors = initialCladeA;
     }
-    const initialCladeD = queryParamMap.get('clade_descendants') === 'true';
+    const initialCladeD = queryParamMap.get('clade_descendants') !== 'false';
     if (initialCladeD) {
       this.search.clade_descendants = initialCladeD;
       this.searchApiOptions.clade_descendants = initialCladeD;
@@ -268,9 +268,7 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     if (this.searchApiOptions.clade_ancestors) {
       queryParams.clade_ancestors = this.searchApiOptions.clade_ancestors;
     }
-    if (this.searchApiOptions.clade_descendants) {
-      queryParams.clade_descendants = this.searchApiOptions.clade_descendants;
-    }
+    queryParams.clade_descendants = this.searchApiOptions.clade_descendants;
     if (this.searchApiOptions.keywords) {
       queryParams.keywords = this.searchApiOptions.keywords;
     }
@@ -293,7 +291,7 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     this.search.classification = null;
     this.search.clade = null;
     this.search.clade_ancestors = false;
-    this.search.clade_descendants = false;
+    this.search.clade_descendants = true;
     this.search.keywords = null;
     this.search.include_raw = false;
     this.searchChanged();

commit 86e95632fa6c91bfd5f9bbbf75d7c8afbcb95615

fix: resolve a change-after-check error in browse panel
This was accidentally introduced in df235fe (checking 'Descendants' box by default).

src/app/shared/browse-panel/browse-panel.component.html
@@ -160,7 +160,7 @@
     </div>
 
     <mat-paginator
-      [pageSize]="20" [pageSizeOptions]="[20,50,100]"
+      [pageSize]="pageSize" [pageIndex]="pageIndex" [pageSizeOptions]="[20,50,100]"
       [length]="families?.total_count" (page)="pageChanged($event)">
     </mat-paginator>
   </div>

src/app/shared/browse-panel/browse-panel.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, AfterViewInit, ViewChild, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { Subject, Subscription, forkJoin } from 'rxjs';
 import { debounceTime, map } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSort, Sort, SortDirection } from '@angular/material/sort';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { PageEvent } from '@angular/material/paginator';
 import { FamilyRepository, ClassesRepository, TaxaRepository } from '../dfam-api/common';
 
 function preg_quote( str ) {
@@ -37,7 +37,7 @@ function has_duplicates(array: any[]) {
   templateUrl: './browse-panel.component.html',
   styleUrls: ['./browse-panel.component.scss']
 })
-export class BrowsePanelComponent implements OnInit, AfterViewInit {
+export class BrowsePanelComponent implements OnInit {
 
   @Input() repository: FamilyRepository & ClassesRepository & TaxaRepository;
   @Input() isEditing: boolean;
@@ -45,6 +45,8 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
   families: any = {};
 
   search: any = {};
+  pageSize: number = 20;
+  pageIndex: number = 0;
   searchApiOptions: any = { };
 
   classOptions: any[] = [];
@@ -67,7 +69,6 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
 
   displayColumns = [ 'accession', 'name', 'classification', 'clades', 'title', 'length' ];
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(
@@ -158,9 +159,7 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     this.updateUrlTask.pipe(debounceTime(300)).subscribe(() => {
       this.updateUrl();
     });
-  }
 
-  ngAfterViewInit() {
     this.restoreSearch();
   }
 
@@ -234,8 +233,8 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     const initialP = parseInt(this.route.snapshot.queryParamMap.get('page'), 10);
     if (initialP) {
       const initialPS = parseInt(this.route.snapshot.queryParamMap.get('pageSize'), 10);
-      this.paginator.pageIndex = initialP;
-      this.paginator.pageSize = initialPS;
+      this.pageIndex = initialP;
+      this.pageSize = initialPS;
     }
     const initialSort = this.route.snapshot.queryParamMap.get('sort');
     if (initialSort) {
@@ -263,7 +262,7 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     this.searchApiOptions.keywords = this.search.keywords;
     this.searchApiOptions.include_raw = this.search.include_raw;
 
-    this.paginator.pageIndex = 0;
+    this.pageIndex = 0;
     this.updateUrlTask.next();
     this.getFamilies();
   }
@@ -290,9 +289,9 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     if (this.searchApiOptions.include_raw) {
       queryParams.include_raw = this.searchApiOptions.include_raw;
     }
-    if (this.paginator.pageIndex) {
-      queryParams.pageSize = this.paginator.pageSize;
-      queryParams.page = this.paginator.pageIndex;
+    if (this.pageIndex > 0) {
+      queryParams.pageSize = this.pageSize;
+      queryParams.page = this.pageIndex;
     }
     if (this.searchApiOptions.sort) {
       queryParams.sort = this.searchApiOptions.sort;
@@ -344,6 +343,8 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
   }
 
   pageChanged(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
     this.updateUrlTask.next();
     this.getFamilies();
   }
@@ -352,8 +353,8 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     if (this.getFamiliesSubscription) {
       this.getFamiliesSubscription.unsubscribe();
     }
-    this.searchApiOptions.limit = this.paginator.pageSize;
-    this.searchApiOptions.start = this.paginator.pageSize * this.paginator.pageIndex;
+    this.searchApiOptions.limit = this.pageSize;
+    this.searchApiOptions.start = this.pageSize * this.pageIndex;
     this.getFamiliesSubscription = this.repository.getFamilies(this.searchApiOptions).subscribe(data => {
       this.disableDownload = (data.total_count <= 0 || data.total_count > 2000);
       for (const format of ['hmm', 'embl', 'fasta']) {