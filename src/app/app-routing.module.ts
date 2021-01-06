cfaa04711e3903bd3e9e7494f8b603fbe0d720ba
Jeb Rosen <jeb.rosen@isbscience.org>
2021-01-06 13:38:56 -0800

    maint: update to Angular 11 (core, CLI)
    `ng update @angular/core @angular/cli`

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -82,7 +82,7 @@ const routes: Routes = [
 //  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
