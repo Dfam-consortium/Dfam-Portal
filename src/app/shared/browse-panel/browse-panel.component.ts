Dfam-consortium/Dfam-Portal

commit 86e95632fa6c91bfd5f9bbbf75d7c8afbcb95615

fix: resolve a change-after-check error in browse panel
This was accidentally introduced in df235fe (checking 'Descendants' box by default).

src/app/shared/browse-panel/browse-panel.component.html
@@ -160,7 +160,7 @@
     </div>
 
     <mat-paginator
-      [pageSize]="20" [pageSizeOptions]="[20,50,100]"
+      [pageSize]="pageSize" [pageIndex]="pageIndex" [pageSizeOptions]="[20,50,100]"
       [length]="families?.total_count" (page)="pageChanged($event)">
     </mat-paginator>
   </div>

src/app/shared/browse-panel/browse-panel.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, AfterViewInit, ViewChild, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { Subject, Subscription, forkJoin } from 'rxjs';
 import { debounceTime, map } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSort, Sort, SortDirection } from '@angular/material/sort';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { PageEvent } from '@angular/material/paginator';
 import { FamilyRepository, ClassesRepository, TaxaRepository } from '../dfam-api/common';
 
 function preg_quote( str ) {
@@ -37,7 +37,7 @@ function has_duplicates(array: any[]) {
   templateUrl: './browse-panel.component.html',
   styleUrls: ['./browse-panel.component.scss']
 })
-export class BrowsePanelComponent implements OnInit, AfterViewInit {
+export class BrowsePanelComponent implements OnInit {
 
   @Input() repository: FamilyRepository & ClassesRepository & TaxaRepository;
   @Input() isEditing: boolean;
@@ -45,6 +45,8 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
   families: any = {};
 
   search: any = {};
+  pageSize: number = 20;
+  pageIndex: number = 0;
   searchApiOptions: any = { };
 
   classOptions: any[] = [];
@@ -67,7 +69,6 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
 
   displayColumns = [ 'accession', 'name', 'classification', 'clades', 'title', 'length' ];
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(
@@ -158,9 +159,7 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     this.updateUrlTask.pipe(debounceTime(300)).subscribe(() => {
       this.updateUrl();
     });
-  }
 
-  ngAfterViewInit() {
     this.restoreSearch();
   }
 
@@ -234,8 +233,8 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     const initialP = parseInt(this.route.snapshot.queryParamMap.get('page'), 10);
     if (initialP) {
       const initialPS = parseInt(this.route.snapshot.queryParamMap.get('pageSize'), 10);
-      this.paginator.pageIndex = initialP;
-      this.paginator.pageSize = initialPS;
+      this.pageIndex = initialP;
+      this.pageSize = initialPS;
     }
     const initialSort = this.route.snapshot.queryParamMap.get('sort');
     if (initialSort) {
@@ -263,7 +262,7 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     this.searchApiOptions.keywords = this.search.keywords;
     this.searchApiOptions.include_raw = this.search.include_raw;
 
-    this.paginator.pageIndex = 0;
+    this.pageIndex = 0;
     this.updateUrlTask.next();
     this.getFamilies();
   }
@@ -290,9 +289,9 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     if (this.searchApiOptions.include_raw) {
       queryParams.include_raw = this.searchApiOptions.include_raw;
     }
-    if (this.paginator.pageIndex) {
-      queryParams.pageSize = this.paginator.pageSize;
-      queryParams.page = this.paginator.pageIndex;
+    if (this.pageIndex > 0) {
+      queryParams.pageSize = this.pageSize;
+      queryParams.page = this.pageIndex;
     }
     if (this.searchApiOptions.sort) {
       queryParams.sort = this.searchApiOptions.sort;
@@ -344,6 +343,8 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
   }
 
   pageChanged(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
     this.updateUrlTask.next();
     this.getFamilies();
   }
@@ -352,8 +353,8 @@ export class BrowsePanelComponent implements OnInit, AfterViewInit {
     if (this.getFamiliesSubscription) {
       this.getFamiliesSubscription.unsubscribe();
     }
-    this.searchApiOptions.limit = this.paginator.pageSize;
-    this.searchApiOptions.start = this.paginator.pageSize * this.paginator.pageIndex;
+    this.searchApiOptions.limit = this.pageSize;
+    this.searchApiOptions.start = this.pageSize * this.pageIndex;
     this.getFamiliesSubscription = this.repository.getFamilies(this.searchApiOptions).subscribe(data => {
       this.disableDownload = (data.total_count <= 0 || data.total_count > 2000);
       for (const format of ['hmm', 'embl', 'fasta']) {

commit 07e28815c450c5099ea8dac0248c6fbf51dd89e5

fix: error in browse panel when sort direction is initially present in query params
'this.sort', an @ViewChild, is undefined until the first render of the
component and can not be accessed during initialization.

This problem was introduced in 86e9563, which fixed a change-after-check
bug with pagination but left a use-before-ready bug with sorting. With
this commit, both are handled the same way without using @ViewChild.

src/app/shared/browse-panel/browse-panel.component.html
@@ -95,7 +95,10 @@
 </mat-expansion-panel>
 
 <div class="browse">
-  <table mat-table matSort (matSortChange)="sortChanged($event)"
+  <table mat-table matSort
+        [matSortActive]="sortActive"
+        [matSortDirection]="sortDirection"
+        (matSortChange)="sortChanged($event)"
         [dataSource]="families.results">
     <ng-container matColumnDef="accession">
       <th mat-header-cell *matHeaderCellDef mat-sort-header>Accession</th>

src/app/shared/browse-panel/browse-panel.component.ts
@@ -48,6 +48,8 @@ export class BrowsePanelComponent implements OnInit {
   families: any = {};
 
   search: any = {};
+  sortActive: string = null;
+  sortDirection: SortDirection = 'asc';
   pageSize = 20;
   pageIndex = 0;
   searchApiOptions: any = { };
@@ -71,8 +73,6 @@ export class BrowsePanelComponent implements OnInit {
   private getFamiliesSubscription: Subscription;
 
   displayColumns = [ 'accession', 'name', 'classification', 'clades', 'title', 'length' ];
-
-  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private route: ActivatedRoute,
@@ -243,8 +243,8 @@ export class BrowsePanelComponent implements OnInit {
     if (initialSort) {
       const parts = initialSort.split(':');
       if (parts.length === 2) {
-        this.sort.active = parts[0];
-        this.sort.direction = parts[1] as SortDirection;
+        this.sortActive = parts[0];
+        this.sortDirection = parts[1] as SortDirection;
         this.searchApiOptions.sort = initialSort;
       }
     }