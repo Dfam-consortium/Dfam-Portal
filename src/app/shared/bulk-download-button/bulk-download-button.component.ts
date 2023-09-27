f82d1b4ccf9648f6bfef1bef33fa55f622ab9687
asgray <antgray89@gmail.com>
2023-09-26 17:04:01 -0700

    download buttons with logic

--- src/app/shared/bulk-download-button/bulk-download-button.component.ts
+++ src/app/shared/bulk-download-button/bulk-download-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'dfam-bulk-download-button',
@@ -10,9 +11,44 @@ export class BulkDownloadButtonComponent implements OnInit {
   @Input() downloadUrl: string;
   @Input() label: string;
 
-  constructor() { }
+  status: string;
+  download: string;
+  type: string;
+  waiting: boolean;
+  reloadInterval: number = 5;
+
+  constructor(
+    private http: HttpClient
+  ) { }
   
   ngOnInit() {
+    this.status = this.label
+    this.type = this.label.toLowerCase()
+    this.waiting = false;
   }
 
+  requestDownload () {
+    this.waiting = true
+    this.status = "Working..."
+    this.http.get(this.downloadUrl, { 
+      observe: 'body', 
+      responseType: 'json'
+    }).subscribe( (data) => {
+      if (data['code'] === 200) {
+        this.waiting = false
+        this.status = this.label
+        this.download = data['body']
+        this.onDownload()
+      } else if (data['code'] === 202) {
+        // TODO
+      }
+    })
+  }
+
+  onDownload() {
+    if (this.download) {
+      const blob = new Blob([this.download], { type: 'text/plain' });
+      window.saveAs(blob, `dfam-${this.type}-download.${this.type}`);
+    }
+  }
 }
