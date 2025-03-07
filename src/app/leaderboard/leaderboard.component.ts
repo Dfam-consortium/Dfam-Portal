6f39a7c03b5e1e916fb84004c6e7722a9697f2ed
asgray <antgray89@gmail.com>
2025-03-06 16:05:22 -0800

    3.9 home data updates

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import dataset from "./dataset.json";
+import dataset from "./dataset_3.9.json"; // this is generated from running /Dfam-umbrella/Server/partition/scripts/getLeaderBoardData.py and hand editing the json
 
 export interface PeriodicElement {
   name: string;
@@ -9,7 +9,7 @@ export interface PeriodicElement {
 @Component({
   selector: 'dfam-leaderboard',
   templateUrl: './leaderboard.component.html',
-  styleUrl: './leaderboard.component.scss'
+  styleUrl: './leaderboard.component.scss',
 })
 export class LeaderboardComponent {
   displayedColumns: string[] = ['count', 'name']; // Column order is defined here. Row order is determined by JSON file order
